2c3f4777c44537fc26dfdd3e4af4dd5342e7a6b7
janaboy74 <janaboy@gmail.com>
2023-11-22 10:43:52 +0100

    Added exception test to project looprecord.

--- looprecord/class/test.cpp
+++ looprecord/class/test.cpp
@@ -1,9 +1,21 @@
 #include <looprecord.h>
 
 //--------------------------------
+int divide( int number, int divider ) {
+//--------------------------------
+    if( divider == 0 )
+        throw "division by zero exception";
+    return number / divider;
+}
+
+//--------------------------------
 void test() {
 //--------------------------------
     loopRecord<int> record( 10 );
+
+    cout << "--------------------\n";
+    cout << "endless record test:\n";
+    cout << "--------------------\n";
     for( int j = 0; j <= 512; ++j ) {
         if( j )
             cout << endl;
@@ -35,8 +47,29 @@ void test() {
         }
         cout << endl;
     }
+
+    cout << "\n---------------\n";
+    cout << "exception text:\n";
+    cout << "---------------\n";
+    record.reset();
+    for( int i = 0 ; i < 42; ++i ) {
+        record.add( i );
+        try {
+            int z = divide( 42, i - 21 );
+        } catch ( const char *error ) {
+            cout << "problem( " << error << " ) with loop at " << i << endl;
+            record.reorg();
+            for( int i = 0; i < record.length(); ++i ) {
+                if( i )
+                    cout << ", ";
+                cout << record[ i ];
+            }
+            cout << endl;
+        }
+    }
     cout.flush();
 }
+
 
 //--------------------------------
 int main(int argc, char *argv[])
--- looprecord/single/looprecord.cpp
+++ looprecord/single/looprecord.cpp
@@ -69,9 +69,21 @@ public:
 };
 
 //--------------------------------
+int divide( int number, int divider ) {
+//--------------------------------
+    if( divider == 0 )
+        throw "division by zero exception";
+    return number / divider;
+}
+
+//--------------------------------
 void test() {
 //--------------------------------
     loopRecord<int> record( 10 );
+
+    cout << "--------------------\n";
+    cout << "endless record test:\n";
+    cout << "--------------------\n";
     for( int j = 0; j <= 512; ++j ) {
         if( j )
             cout << endl;
@@ -102,6 +114,26 @@ void test() {
             prev = cur;
         }
         cout << endl;
+    }
+
+    cout << "\n---------------\n";
+    cout << "exception text:\n";
+    cout << "---------------\n";
+    record.reset();
+    for( int i = 0 ; i < 42; ++i ) {
+        record.add( i );
+        try {
+            int z = divide( 42, i - 21 );
+        } catch ( const char *error ) {
+            cout << "problem( " << error << " ) with loop at " << i << endl;
+            record.reorg();
+            for( int i = 0; i < record.length(); ++i ) {
+                if( i )
+                    cout << ", ";
+                cout << record[ i ];
+            }
+            cout << endl;
+        }
     }
     cout.flush();
 }
