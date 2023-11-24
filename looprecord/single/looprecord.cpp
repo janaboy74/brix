1f39a012cf903cb4ad2844ac0212a23b420f11d2
janaboy74 <janaboy@gmail.com>
2023-11-24 10:17:52 +0100

    Single looprecord test extended.

--- looprecord/single/looprecord.cpp
+++ looprecord/single/looprecord.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <cstring>
+#include <cstdarg>
+
+#define arrayLen( array ) ( sizeof( array ) / sizeof( *array ))
 
 using namespace std;
 
@@ -68,6 +72,75 @@ public:
     }
 };
 
+//-----------------------
+struct corestring : public std::string {
+//-----------------------
+    corestring() : std::string() {}
+    corestring( const std::string &src ) : std::string( src ) {}
+    corestring( const char *src ) : std::string( src ) {}
+    corestring( const char src ) : string( &src, 1 ) {}
+    void formatva( const char *format, va_list &arg_list ) {
+        if( format ) {
+            va_list cova = {{}};
+            va_copy( cova, arg_list );
+            int size = vsnprintf( nullptr, 0, format, cova );
+            va_end( arg_list );
+            resize( size );
+            va_copy( cova, arg_list );
+            vsnprintf( &at( 0 ), size + 1, format, cova );
+            va_end( arg_list );
+        }
+    }
+    void format( const char *format, ... ) {
+        if( format ) {
+            va_list arg_list;
+            va_start( arg_list, format );
+            formatva( format, arg_list );
+            va_end( arg_list );
+        }
+    }
+    int toInt() const {
+        return atoi( c_str() );
+    }
+    long toLong() const {
+        return atol( c_str() );
+    }
+    float toFloat() const {
+        return atof( c_str() );
+    }
+    double toDouble() const {
+        return atof( c_str() );
+    }
+    char *get() {
+        return &*begin();
+    }
+    bool begins( const char *start ) const {
+        return !strncmp( c_str(), start, strlen( start ));
+    }
+    void operator += ( const char * append ) {
+        auto length = strlen( append );
+        auto prevSize = size();
+        resize( prevSize + length );
+        strncpy(( char *) &*begin() + prevSize, append, length );
+    }
+    operator const char *() const {
+        return c_str();
+    };
+};
+
+//-----------------------
+uint32_t get_rnd( uint32_t min, uint32_t max ) {
+//-----------------------
+    static unsigned int hash = clock();
+    hash *= 0x372ce9b9;
+    hash += 0xb9e92c37;
+    if( min > max )
+        return max;
+    return min + (( hash << 7 ) ^ ( hash >> 9 )  >> 1 ) % ( max - min );
+}
+
+const char *syllables[]={ "no", "ki", "us", "la", "er", "ot", "qa", "ew", "tu", "ba", "uz", "ce" };
+
 //--------------------------------
 int divide( int number, int divider ) {
 //--------------------------------
@@ -135,6 +208,33 @@ void test() {
             cout << endl;
         }
     }
+
+    loopRecord<corestring> csrecord( 5 );
+
+    cout << "---------------\n";
+    cout << " --- corestring test ---\n";
+    for( int e = 3 ; e < 17; ++e ) {
+        corestring out;
+        const size_t maxWords = get_rnd( 7, 11 );
+        for( size_t i = 0; i < maxWords; ++i ) {
+            if( i )
+                out += " ";
+            const size_t maxSyllable = get_rnd( 1, 5 );
+            for( size_t j = 0; j < maxSyllable; ++j ) {
+                out += syllables[ get_rnd( 0, arrayLen( syllables ))];
+            }
+        }
+        out[ 0 ] = toupper( out[ 0 ]);
+        out += ".";
+        csrecord.add( out );
+        cout << out << endl;
+    }
+    csrecord.reorg();
+    cout << " --- reorded ---\n";
+    for( int i = 0; i < csrecord.length(); ++i ) {
+        cout << csrecord[ i ] << endl;
+    }
+
     cout.flush();
 }
 
